0768085c850d89e4303c92039863fe2afe7f87fd
Cole Shapiro <colesayershapiro@gmail.com>
2017-08-09 17:26:57 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    modified_element = yield collection[i]
+    new_collection << modified_element
+    i += 1
+  end
+  new_collection
+end
 
+collection = []
 
+my_collect(collection) do |x|
+  x.split(" ").first
+end
+
+my_collect(collection) do |x|
+  x.upcase
+end
